0288bccc71cf5f5d14a2c1af3f2d68406fd036f0
Michelle Plur <52613476+wendydv1989@users.noreply.github.com>
2021-08-31 00:31:26 +0200

    Update paymentSplitterTest.js

--- test/paymentSplitterTest.js
+++ test/paymentSplitterTest.js
@@ -21,6 +21,8 @@ const swarmcityShare = 2879
 const aeternityShare = 5371
 const edgelessShare = 1751
 
+let txCost
+
 describe("PaymentSplitter", function () {
     it("Deploy contract with payees and shares set", async function () {
         [deployer, swarmcity, aeternity, edgeless, hacker] = await ethers.getSigners();
@@ -41,9 +43,6 @@ describe("PaymentSplitter", function () {
 
         paymentSplitterBalance = await provider.getBalance(paymentSplitter.address)
 
-        console.log((await provider.getBalance(paymentSplitter.address)).toString())
-        console.log((await hacker.getBalance()).toString())
-
         expect(await paymentSplitter.payee(0)).to.equal(swarmcity.address);
         expect(await paymentSplitter.payee(1)).to.equal(aeternity.address);
         expect(await paymentSplitter.payee(2)).to.equal(edgeless.address);
@@ -51,32 +50,59 @@ describe("PaymentSplitter", function () {
         expect(await paymentSplitter.shares(swarmcity.address)).to.equal(swarmcityShare);
         expect(await paymentSplitter.shares(aeternity.address)).to.equal(aeternityShare);
         expect(await paymentSplitter.shares(edgeless.address)).to.equal(edgelessShare);
-
     });
 
-    it("Send 500 eth from hacker account to payment splitter", async function () {
-        await hacker.sendTransaction({
+    it("Hacker sends 500 eth to paymentSplitter", async function () {
+        const tx = await hacker.sendTransaction({
             from: hacker.address,
             to: paymentSplitter.address,
-            value: ethers.utils.parseEther("1"),
+            value: ethers.utils.parseEther("500"),
         });
 
-        console.log((await provider.getBalance(paymentSplitter.address)).toString())
-        console.log((await hacker.getBalance()).toString())
+        txCost = tx.gasLimit.mul(tx.gasPrice)
 
-        //console.log(paymentSplitterBalance)
-        console.log((await provider.getBalance(paymentSplitter.address)).toString())
-
-        //expect(paymentSplitterBalance).to.equal(paymentSplitterBalance.add(ethers.utils.parseEther("500")));
+        expect(tx.to.exist)
     });
 
-    //   it("accounts ask for payout", async function(){
-    //     const tx1 = await paymentSplitter.connect(swarmcity).release(swarmcity.address)
-    //     const tx2 = await paymentSplitter.connect(aeternity).release(aeternity.address)
-    //     const tx3 = await paymentSplitter.connect(edgeless).release(edgeless.address)
-    //     paymentSplitterBalance = (await provider.getBalance(paymentSplitter.address)).toString()
-    //     console.log(paymentSplitterBalance)
-    //     expect((await swarmcity.getBalance()).toString()).to.equal(1000)
-    //   });
+    it("Hacker balance is correct", async function () {
+        // so the hacker just paid gas cost and 500 eth
+        const txValue = txCost.add(ethers.utils.parseEther("500"))
+        const hackerBalanceCheck = hackerBalance
+        expect(hackerBalanceCheck.sub(txValue)).to.equal(await hacker.getBalance())
+    });
+
+    it("Swarm.city requests payout", async function () {
+        const tx1 = await paymentSplitter.connect(swarmcity).release(swarmcity.address)
+        expect(tx1.to.exist)
+    });
+
+    it("Swarm.city balance is correct", async function () {
+        expect((await swarmcity.getBalance()).toString()).to.equal("10143935470423257334573")// 10143935470423257334573
+    });
+
+    it("Aeternity requests payout", async function () {
+        const tx2 = await paymentSplitter.connect(aeternity).release(aeternity.address)
+        expect(tx2.to.exist)
+    });
+
+    it("Aeternity balance is correct", async function () {
+        expect((await aeternity.getBalance()).toString()).to.equal("10268523045737810969220")// 10143935470423257334573
+    });
+
+    it("Edgeless requests payout", async function () {
+        const tx3 = await paymentSplitter.connect(edgeless).release(edgeless.address)
+        expect(tx3.to.exist)
+    });
+
+    it("Edgeless balance is correct", async function () {
+        expect((await edgeless.getBalance()).toString()).to.equal("10087541148629464728394")// 10143935470423257334573
+    });
+
+    // it("PaymentSplitter balance is correct", async function () {
+    //     expect((await provider.getBalance(paymentSplitter.address)).toString()).to.equal("1022701630000000001")// 10143935470423257334573
+    // });
+
+
+
 
 });
