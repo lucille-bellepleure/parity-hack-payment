8c30b33c8f0f4a21deed6ea105b132e1e4b29280
Michelle Plur <52613476+wendydv1989@users.noreply.github.com>
2021-08-30 22:45:40 +0200

    Update paymentSplitterTest.js

--- test/paymentSplitterTest.js
+++ test/paymentSplitterTest.js
@@ -2,44 +2,81 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 let provider;
+
 let paymentSplitter;
+
 let deployer;
-let account1;
-let account2;
-let account3;
-let account4;
+let swarmcity;
+let aeternity;
+let edgeless;
+let hacker;
+
+let paymentSplitterBalance;
+let swarmcityBalance;
+let aeternityBalance;
+let edgelessBalance;
+let hackerBalance;
+
+const swarmcityShare = 2879
+const aeternityShare = 5371
+const edgelessShare = 1751
 
 describe("PaymentSplitter", function () {
-  it("Should deploy, set the payees and shares.", async function () {
-    [deployer, account1, account2, account3, account4] = await ethers.getSigners();
-    provider = ethers.getDefaultProvider()
-    console.log("Deploying contracts with the account:", deployer.address);
-    console.log("Account balance:", (await deployer.getBalance()).toString());
-  
-    const payees = [account1.address, account2.address, account3.address]
-    const shares = [12,24,64]
-    const PaymentSplitter = await ethers.getContractFactory("PaymentSplitter");
-    paymentSplitter = await PaymentSplitter.deploy(payees, shares);
-    await paymentSplitter.deployed();
+    it("Deploy contract with payees and shares set", async function () {
+        [deployer, swarmcity, aeternity, edgeless, hacker] = await ethers.getSigners();
+        provider = ethers.getDefaultProvider()
 
-    expect(await paymentSplitter.payee(0)).to.equal(account1.address);
-    expect(await paymentSplitter.payee(1)).to.equal(account2.address);
-    expect(await paymentSplitter.payee(2)).to.equal(account3.address);
+        swarmcityBalance = await swarmcity.getBalance()
+        aeternityBalance = await aeternity.getBalance()
+        edgelessBalance = await edgeless.getBalance()
+        hackerBalance = await hacker.getBalance()
 
-  });
-  it("Should send eth from account4", async function () {
-    await account4.sendTransaction({
-        from: account4.address,
-        to:paymentSplitter.address,
-        value: ethers.utils.parseEther("1000"), // Sends exactly 1.0 ether
-      });
-      expect(await paymentSplitter.payee(0)).to.equal(account1.address);
-    expect(await provider.getBalance(paymentSplitter.address)).to.equal(900);
-  });
-  it("accounts ask for payout", async function(){
-    const tx1 = await paymentSplitter.connect(account1).release(account1.address)
-    const tx2 = await paymentSplitter.connect(account2).release(account2.address)
-    const tx3 = await paymentSplitter.connect(account3).release(account3.address)
-  });
-  
+        const payees = [swarmcity.address, aeternity.address, edgeless.address]
+        const shares = [swarmcityShare, aeternityShare, edgelessShare]
+
+        const PaymentSplitter = await ethers.getContractFactory("PaymentSplitter");
+        paymentSplitter = await PaymentSplitter.deploy(payees, shares);
+
+        await paymentSplitter.deployed();
+
+        paymentSplitterBalance = await provider.getBalance(paymentSplitter.address)
+
+        console.log((await provider.getBalance(paymentSplitter.address)).toString())
+        console.log((await hacker.getBalance()).toString())
+
+        expect(await paymentSplitter.payee(0)).to.equal(swarmcity.address);
+        expect(await paymentSplitter.payee(1)).to.equal(aeternity.address);
+        expect(await paymentSplitter.payee(2)).to.equal(edgeless.address);
+
+        expect(await paymentSplitter.shares(swarmcity.address)).to.equal(swarmcityShare);
+        expect(await paymentSplitter.shares(aeternity.address)).to.equal(aeternityShare);
+        expect(await paymentSplitter.shares(edgeless.address)).to.equal(edgelessShare);
+
+    });
+
+    it("Send 500 eth from hacker account to payment splitter", async function () {
+        await hacker.sendTransaction({
+            from: hacker.address,
+            to: paymentSplitter.address,
+            value: ethers.utils.parseEther("1"),
+        });
+
+        console.log((await provider.getBalance(paymentSplitter.address)).toString())
+        console.log((await hacker.getBalance()).toString())
+
+        //console.log(paymentSplitterBalance)
+        console.log((await provider.getBalance(paymentSplitter.address)).toString())
+
+        //expect(paymentSplitterBalance).to.equal(paymentSplitterBalance.add(ethers.utils.parseEther("500")));
+    });
+
+    //   it("accounts ask for payout", async function(){
+    //     const tx1 = await paymentSplitter.connect(swarmcity).release(swarmcity.address)
+    //     const tx2 = await paymentSplitter.connect(aeternity).release(aeternity.address)
+    //     const tx3 = await paymentSplitter.connect(edgeless).release(edgeless.address)
+    //     paymentSplitterBalance = (await provider.getBalance(paymentSplitter.address)).toString()
+    //     console.log(paymentSplitterBalance)
+    //     expect((await swarmcity.getBalance()).toString()).to.equal(1000)
+    //   });
+
 });
