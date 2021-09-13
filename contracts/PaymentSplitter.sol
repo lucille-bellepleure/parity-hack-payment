f36b23da613d0f766aedb4e49664b2e562fa86ea
Nikita Fuchs <q.fuchs@gmail.com>
2021-09-13 09:42:36 +0200

    add Æternity's wallet to hardcoded payees list

--- contracts/PaymentSplitter.sol
+++ contracts/PaymentSplitter.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 
 contract _PaymentSplitter is PaymentSplitter {
     address[] payees = [
-        0xe3b724C8b15BfE5A7d796F0A9C50617F5678CBDa,
+        0x3A753b048bec30Ada977E47ea78D17684C82295e, // Æternity's wallet
         0x12B1b58d7f28f116D505ECC48C436D4381793bd5,
         0x73A89F5E8fe75c873e85664eb3C1Ba69082C0890
     ];
