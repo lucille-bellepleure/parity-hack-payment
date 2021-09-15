0f31c67ce5763e5e8c0d12b08ea91db9c76c500b
Michelle Plur <52613476+wendydv1989@users.noreply.github.com>
2021-09-16 00:07:39 +0200

    Update PaymentSplitter.sol
    added edgeless address

--- contracts/PaymentSplitter.sol
+++ contracts/PaymentSplitter.sol
@@ -6,7 +6,7 @@ contract _PaymentSplitter is PaymentSplitter {
     address[] payees = [
         0x3A753b048bec30Ada977E47ea78D17684C82295e, // Æternity's wallet
         0x50126e8Fcb9Be29f83c6bBd913CC85b40EAf86fC, // Swarm.city wallet
-        0x0 // Edgeless wallet
+        0x990CB6E4CAE60A4D4Ff5ED99b92c0cefDc3547dC // Edgeless wallet
     ];
     uint256[] shares_ = [537053, 287872, 175075];
 
