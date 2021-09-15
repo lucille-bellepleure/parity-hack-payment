c43237abf170406109d9ddcd8ad27d09cd37c5c8
Michelle Plur <52613476+wendydv1989@users.noreply.github.com>
2021-09-15 17:29:43 +0200

    Update PaymentSplitter.sol

--- contracts/PaymentSplitter.sol
+++ contracts/PaymentSplitter.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/finance/PaymentSplitter.sol";
 contract _PaymentSplitter is PaymentSplitter {
     address[] payees = [
         0x3A753b048bec30Ada977E47ea78D17684C82295e, // Æternity's wallet
-        0x12B1b58d7f28f116D505ECC48C436D4381793bd5,
-        0x73A89F5E8fe75c873e85664eb3C1Ba69082C0890
+        0x50126e8Fcb9Be29f83c6bBd913CC85b40EAf86fC, // Swarm.city wallet
+        0x0 // Edgeless wallet
     ];
-    uint256[] shares_ = [2879, 1751, 5370];
+    uint256[] shares_ = [537053, 287872, 175075];
 
     constructor() PaymentSplitter(payees, shares_) {}
 }
